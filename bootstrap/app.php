5c5833fe19ee185aa166e107f3b34e436baf796f
Niariasve <niarias@espol.edu.ec>
2026-02-22 19:30:10 -0500

    🐛 Fix set locale middleware position

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,9 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleAppearance::class,
+            SetLocaleMiddleware::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-            SetLocaleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
