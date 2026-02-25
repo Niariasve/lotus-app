0af3284003433d274b76fb6dfa14bd358b6be132
Niariasve <niarias@espol.edu.ec>
2026-02-25 11:10:38 -0500

    ✨ Add data table component

---
+++ resources/js/components/ui/data-table/DataTablePagination.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts" generic="TData">
+  import { type Table } from '@tanstack/vue-table'
+
+  import { ChevronLeft, ChevronRight, ArrowLeftToLine, ArrowRightToLine } from 'lucide-vue-next'
+
+  import { Button } from '@/components/ui/button'
+  import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from '@/components/ui/select'
+
+  interface DataTablePaginationProps<TData> {
+    table: Table<TData>
+  }
+  defineProps<DataTablePaginationProps<TData>>()
+</script>
+
+<template>
+  <div class="flex flex-col sm:flex-row items-center justify-between px-2">
+    <div class="flex-1 text-sm text-muted-foreground sm:mr-2">
+      {{ table.getFilteredSelectedRowModel().rows.length }} of
+      {{ table.getFilteredRowModel().rows.length }} row(s) selected.
+    </div>
+    <div class="flex flex-col sm:flex-row items-center space-y-2 md:space-y-0 space-x-6 lg:space-x-8">
+      <div class="flex items-center space-x-2">
+        <p class="text-sm font-medium">
+          Rows per page
+        </p>
+        <Select 
+          :model-value="`${table.getState().pagination.pageSize}`" 
+          @update:model-value="(value) => table.setPageSize(Number(value))"
+        >
+          <SelectTrigger class="h-8 w-[70px]">
+            <SelectValue :placeholder="`${table.getState().pagination.pageSize}`" />
+          </SelectTrigger>
+          <SelectContent side="top">
+            <SelectItem v-for="pageSize in [10, 20, 30, 40, 50]" :key="pageSize" :value="`${pageSize}`">
+              {{ pageSize }}
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <div class="flex w-[100px] items-center justify-center text-sm font-medium">
+        Page {{ table.getState().pagination.pageIndex + 1 }} of
+        {{ table.getPageCount() }}
+      </div>
+      <div class="flex items-center space-x-2">
+        <Button variant="outline" class="hidden w-8 h-8 p-0 lg:flex" :disabled="!table.getCanPreviousPage()"
+          @click="table.setPageIndex(0)">
+          <span class="sr-only">Go to first page</span>
+          <ArrowLeftToLine class="w-4 h-4" />
+        </Button>
+        <Button variant="outline" class="w-8 h-8 p-0" :disabled="!table.getCanPreviousPage()"
+          @click="table.previousPage()">
+          <span class="sr-only">Go to previous page</span>
+          <ChevronLeft class="w-4 h-4" />
+        </Button>
+        <Button variant="outline" class="w-8 h-8 p-0" :disabled="!table.getCanNextPage()" @click="table.nextPage()">
+          <span class="sr-only">Go to next page</span>
+          <ChevronRight class="w-4 h-4" />
+        </Button>
+        <Button variant="outline" class="hidden w-8 h-8 p-0 lg:flex" :disabled="!table.getCanNextPage()"
+          @click="table.setPageIndex(table.getPageCount() - 1)">
+          <span class="sr-only">Go to last page</span>
+          <ArrowRightToLine class="w-4 h-4" />
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
