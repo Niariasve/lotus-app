949d10900896b44a3dbb496540859d19a1283c81
Niariasve <niarias@espol.edu.ec>
2026-03-05 18:17:36 -0500

    ✨ Products Delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -78,6 +78,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        
+        return back();
     }
 }
